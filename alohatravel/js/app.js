09e935852ecff688a8c66d192c5ce2655894f6cf
Toby Ziegler - MacBook <MacZiegler@gmail.com>
2018-03-25 21:43:40 -0500

    delete example files, update logos, unsuccessfully attempt angularJS

--- alohatravel/js/app.js
+++ alohatravel/js/app.js
@@ -1,17 +1,36 @@
-var myApp = angular.module('myApp', [
-  'ngRoute',
-  'myControllers'
-]);
 
-myApp.config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'js/partials/search.html',
-      controller: 'SearchController'
-    })
-    .when('/details/:itemId', {
-      templateUrl: 'js/partials/details.html',
-      controller: 'DetailsController'
-    });
+// var app = angular.module('alohaApp', [])
+// app.controller('JourneyController', function ($scope) {
+//     $scope.journey = {
+//       "longname": "Brisbane, Australia",
+//       "shortname": "Brisbane",
+//       "activities": [ "City Tours", "Sports", "Cycling", "Museums", "Boating" ]
+//     };
+// });
+
+var app = angular.module('alohaApp', [])
+app.controller('JourneyController', function ($scope, $http) {
+  $http.get('js/data.json').then(function (response) {
+    $scope.journey = response.data.records;
+  });
+});
+
+angular.module('filters.stringUtils', [])
+
+.filter('nospaces', [function () {
+  return function (value) {
+    return (!value) ? '' : value.replace(/ /g, '');
+  };
 }]);
+
+// angular.module('filters.stringUtils', [])
+
+// .filter('removeSpaces', [function() {
+//     return function(string) {
+//         if (!angular.isString(string)) {
+//             return string;
+//         }
+//         return string.replace(/[\s]/g, '');
+//     };
+// }])
 
