c93cd4461caf990e414829d028ee358522bfbf20
TobyZiegler <tobyziegler@tobyziegler.com>
2018-03-27 17:32:16 -0500

    add php methods

---
+++ post_user_data.php
@@ -0,0 +1,30 @@
+<?php
+$errors = array();
+$data = array();
+// Getting posted data and decodeing json
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+// checking for blank values.
+if (empty($_POST['firstname']))
+  $errors['firstname'] = 'First name is missing.';
+
+if (empty($_POST['lastname']))
+  $errors['lastname'] = 'Last name is missing.';
+
+if (empty($_POST['email']))
+  $errors['email'] = 'No email is present.';
+
+if (empty($_POST['username']))
+  $errors['username'] = 'No username is entered.';
+
+if (empty($_POST['password']))
+  $errors['password'] = 'Password is missing.';
+
+if (!empty($errors)) {
+  $data['errors']  = $errors;
+} else {
+  $data['message'] = 'Form data is going well';
+}
+// response back.
+echo json_encode($data);
+?>
