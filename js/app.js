2383d1efb8e3a34f477bb908f3f04f3c206649af
TobyZiegler <tobyziegler@tobyziegler.com>
2018-04-18 01:39:40 -0500

    add most of the changes necessary for completion, php still wonky

--- js/app.js
+++ js/app.js
@@ -1,75 +1,92 @@
 var app = angular.module('alohaApp', []);
-app.controller('JourneyController', function ($scope, $http) {
+app.controller('journeyController', function ($scope, $http) {
+
+  // function journeyController($scope, $http) {
+
   $http.get('js/data.json').then(function (response) {
+
     $scope.places = response.data;
-    $scope.selectedplace = {
-      "idno": "0",
-      "shortname": "Anywhere",
-      "longname": "Your vacation awaits...",
-      "filename": "destunknown.jpg",
-      "activities": "",
-      "choices": []
-    };
-    var firstplace = angular.copy($scope.selectedplace);
-
-    $scope.resetForm = function () {
-      $scope.selectedplace = angular.copy(firstplace);
-      $scope.placeform.$setPristine();
-    }
-
-    $scope.isPlaceChanged = function ()
-    {
-      return !angular.equals($scope.selectedplace, firstplace);
-    };
-    
-    $scope.processinput = function () {
-      http({
-        method: 'POST',
-        url: 'post_reserve_data.php',
-        // data: $.param($scope.selectedplace),  // pass in data as strings
-        data: $scope.selectedplace,  // pass in data as strings
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
-      })
-        .success(function (data) {
-          console.log(data);
-
-          if (!data.success) {
-            // if not successful, bind errors to error variables and bind fail message
-            $scope.errorShortname = data.errors.shortname;
-            $scope.errorAdults = data.errors.adults;
-            $scope.errorChildren = data.errors.children;
-            $scope.errorDepart = data.errors.depart;
-            $scope.errorReturn = data.errors.return;
-            $scope.message = data.message;
-            $scope.success = data.success;
-          } else {
-            // if successful, just bind success and message
-            $scope.message = data.message;
-            $scope.success = data.success;
-          }
-        });
-
-    }
   });
-});
-// Super thanks to Alexander Puchkov for the numbers-only directive: https://codepen.io/apuchkov/pen/ILjFr
-app.directive('numbersOnly', function () {
-  return {
-    require: 'ngModel',
-    link: function (scope, element, attr, ngNumberController) {
-      function fromUser(text) {
-        if (text) {
-          var transformedInput = text.replace(/[^0-9]/g, '');
 
-          if (transformedInput !== text) {
-            ngNumberController.$setViewValue(transformedInput);
-            ngNumberController.$render();
-          }
-          return transformedInput;
-        }
-        return undefined;
-      }
-      ngNumberController.$parsers.push(fromUser);
-    }
+  $scope.selectedplace = {
+    "shortname": "Anywhere",
+    "longname": "Your vacation awaits...",
+    "filename": "destunknown.jpg",
+    "activities": "",
+    "choices": []
   };
+
+  $scope.user = {
+    "firstname": "",
+    "lastname": "",
+    "email": "",
+    "username": "",
+    "password": ""
+  };
+
+  var firstplace = angular.copy($scope.selectedplace);
+  var firstperson = angular.copy($scope.user);
+
+  $scope.resetForm = function () {
+    $scope.selectedplace = angular.copy(firstplace);
+    $scope.placeform.$setPristine();
+  };
+
+  $scope.resetInfo = function () {
+    $scope.user = angular.copy(firstperson);
+    $scope.userform.$setPristine();
+  };
+
+  $scope.isPlaceChanged = function () {
+    return !angular.equals($scope.selectedplace, firstplace);
+  };
+
+  $scope.isPersonChanged = function () {
+    return !angular.equals($scope.user, firstperson);
+  };
+
+  $scope.processinput = function () {
+    $http({
+      method: 'POST',
+      url: 'post_reserve_data.php',
+      // data: $.param($scope.selectedplace),  // use jquery to pass in data as strings
+      data: $scope.selectedplace,  // pass in data as object
+      // headers: { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+    })
+      .then(function successCallback(response) {
+        // console.log(response);
+        $scope.message = response.data;
+
+      }, function errorCallback(response) {
+        $scope.errorShortname = response.errors.shortname;
+        $scope.errorAdults = response.errors.adults;
+        $scope.errorChildren = response.errors.children;
+        $scope.errorDepart = response.errors.depart;
+        $scope.errorReturn = response.errors.return;
+        $scope.message = response.data;
+      });
+
+  };
+
+  $scope.processuser = function () {
+    $http({
+      method: 'POST',
+      url: 'post_user_data.php',
+      data: $scope.selectedplace,  // pass in data as object
+    })
+      .then(function successCallback(response) {
+        // console.log(response);
+        $scope.user = response.data;
+
+      }, function errorCallback(response) {
+        $scope.errorFirstname = response.errors.firstname;
+        $scope.errorLastname = response.errors.lastname;
+        $scope.errorEmail = response.errors.email;
+        $scope.errorUsername = response.errors.username;
+        $scope.errorPassword = response.errors.password;
+        $scope.message = response.data;
+      });
+
+  };
+
 });
--- post_user_data.php
+++ post_user_data.php
@@ -22,10 +22,8 @@ if (empty($_POST['password']))
 
 if (!empty($errors)) {
   $data['errors']  = $errors;
-  $data['success'] = false;
 } else {
   $data['message'] = 'Form data is going well';
-  $data['success'] = true;
 }
 // response back.
 echo json_encode($data);
